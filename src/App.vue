a128e38c522ec78b32d5b338d5b96e5805242e1a
finowo <whittaker.fin@gmail.com>
2021-11-11 12:17:29 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <MyNavBar />
+    <b-container>
+      <br />
+      <b-row>
+        <router-view />
+      </b-row>
+      <MyFooter />
+    </b-container>
+  </div>
+</template>
+
+<script>
+import MyNavBar from "@/components/MyNavBar.vue"
+import MyFooter from "@/components/MyFooter.vue"
+
+export default {
+  name: "App",
+  components: {
+    MyNavBar,
+    MyFooter
+  }
+};
+</script>
+
+<style>
+
+</style>
